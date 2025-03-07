d7ed4961b65ac6f4fecf235fca7de12a2ef41097
Gustav Torup Jacobsen <115047220+gustavtjac@users.noreply.github.com>
2025-03-07 10:24:29 +0100

    Add files via upload

---
+++ TouristGuideCreateDBSCRIPT.sql
@@ -0,0 +1,29 @@
+Create DATABASE touristGuideDB;
+USE touristGuideDB;
+CREATE TABLE cities (
+CityName varchar(30) not null,
+CityID int AUTO_INCREMENT PRIMARY KEY 
+);
+
+CREATE TABLE tags(
+TagsID int AUTO_INCREMENT PRIMARY KEY,
+Tag varchar(30) not null
+);
+
+
+CREATE TABLE attractions (
+    AttractionName VARCHAR(30) not null,
+    AttractionDescription VARCHAR(250),
+	CityID int,
+    AttractionID int AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY (CityID) REFERENCES cities(CityID)
+) AUTO_INCREMENT = 1000;
+
+
+CREATE TABLE attraction_tags(
+AttractionsAttractionID int,
+TagsID int,
+PRIMARY KEY(AttractionsAttractionID, TagsID),
+FOREIGN KEY(AttractionsAttractionID) REFeRENCES attractions(AttractionID) ON DELETE CASCADE,
+FOREIGN KEY(TagsID) REFERENCES tags(TagsID) ON DELETE CASCADE
+);
